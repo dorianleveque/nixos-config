efa16116e4362a83922e17521e5a5fe7937fd4bf
Dorian Lévêque <dorian.leveque@outlook.fr>
2026-04-22 12:08:53 +0200

    fix (16-create-a-gaming-profile): Add an option to activate the steam configuration

--- config/features/steam.nix
+++ config/features/steam.nix
@@ -1,20 +1,21 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    steam
-  ];
+  options.features.steam.enable = lib.mkEnableOption "Steam";
 
-  programs.steam = {
-    enable = false;
-    
-    # Open ports in the firewall for Source Dedicated Server
-    dedicatedServer.openFirewall = true; 
-    
-    # Open ports in the firewall for Steam Remote Play
-    remotePlay.openFirewall = true; 
-    
-    # Open ports in the firewall for Steam Local Network Game Transfers
-    localNetworkGameTransfers.openFirewall = true; 
-  };
+  config = lib.mkIf config.features.steam.enable {
+  
+    programs.steam = {
+      enable = true;
+      
+      # Open ports in the firewall for Source Dedicated Server
+      dedicatedServer.openFirewall = true; 
+      
+      # Open ports in the firewall for Steam Remote Play
+      remotePlay.openFirewall = true; 
+      
+      # Open ports in the firewall for Steam Local Network Game Transfers
+      localNetworkGameTransfers.openFirewall = true; 
+    };
+  }
 }
