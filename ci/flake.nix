0c9faecb42152d6512b47634111445dc039f8619
Dorian Lévêque <dorian.leveque@outlook.fr>
2026-04-25 11:45:49 +0200

    feat: add a mini profile (#53)

--- ci/flake.nix
+++ ci/flake.nix
@@ -2,6 +2,13 @@
   inputs.nixos-config.url = "github:dorianleveque/my-nixos-config/main";
   outputs = { nixos-config, ... }: {
     nixosConfigurations = {
+
+      mini = nixos-config.lib.mkSystem {
+        profile = "mini";
+        hardware = ./hardware-configuration.nix;
+        local = ./configuration.nix;
+      };
+
       default = nixos-config.lib.mkSystem {
         profile = "home";
         hardware = ./hardware-configuration.nix;
