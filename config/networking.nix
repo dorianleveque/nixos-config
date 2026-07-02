2d9f453afa729f830ab9d9e5ed6d839c33c6bb1d
Dorian Lévêque <dorian.leveque@outlook.fr>
2026-07-02 23:30:34 +0200

    feat hostname: disable the hostname field in Gnome settings when its managed by the DHCP server (#43)

--- config/networking.nix
+++ config/networking.nix
@@ -1,12 +1,15 @@
 { config, lib, hostname, ... }:
 
+let
+  isDhcpManaged = hostname == "default";
+in
 {
   networking = {
     # Enable networking
     networkmanager.enable = true;
 
     # Define the hostname
-    hostName = lib.mkIf (hostname != "default") hostname;
+    hostName = lib.mkIf (!isDhcpManaged) hostname;
 
     # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
     # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -14,4 +17,17 @@
     # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
     useDHCP = lib.mkDefault true;
   };
+  
+  # Prevents the modification of the hostname via the UI (e.g GNOME Settings)
+  # by a non-root user, only when the hostname is manages by the DHCP server.
+  security.polkit.extraConfig = lib.mkIf isDhcpManaged ''
+    polkit.addRule(function(action, subject) {
+      switch(action.id) {
+        case "org.freedesktop.hostname1.set-static-hostname":
+        case "org.freedesktop.hostname1.set-hostname":
+        case "org.freedesktop.hostname1.set-pretty-hostname":
+          return subject.user === "root" ? polkit.Result.YES : polkit.Result.NO
+      }
+    });
+  '';
 }
