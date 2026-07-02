6ecc2187cee1492a215115e911640a1c7183dbe1
dorianleveque <dorian.leveque@outlook.fr>
2026-07-02 23:27:45 +0200

    feat hostname: fix lint

--- config/networking.nix
+++ config/networking.nix
@@ -16,18 +16,18 @@ in
     # still possible to use this option, but it's recommended to use it in conjunction
     # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
     useDHCP = lib.mkDefault true;
-
-    # Prevents the modification of the hostname via the UI (e.g GNOME Settings)
-    # by a non-root user, only when the hostname is manages by the DHCP server.
-    security.polkit.extraConfig = lib.mkIf isDhcpManaged ''
-      polkit.addRule(function(action, subject) {
-        switch(action.id) {
-          case "org.freedesktop.hostname1.set-static-hostname":
-          case "org.freedesktop.hostname1.set-hostname":
-          case "org.freedesktop.hostname1.set-pretty-hostname":
-            return subject.user === "root" ? polkit.Result.YES : polkit.Result.NO
-        }
-      });
-    '';
   };
+  
+  # Prevents the modification of the hostname via the UI (e.g GNOME Settings)
+  # by a non-root user, only when the hostname is manages by the DHCP server.
+  security.polkit.extraConfig = lib.mkIf isDhcpManaged ''
+    polkit.addRule(function(action, subject) {
+      switch(action.id) {
+        case "org.freedesktop.hostname1.set-static-hostname":
+        case "org.freedesktop.hostname1.set-hostname":
+        case "org.freedesktop.hostname1.set-pretty-hostname":
+          return subject.user === "root" ? polkit.Result.YES : polkit.Result.NO
+      }
+    });
+  '';
 }
